b8f5b08df979642402eaed5b306f5b175adcec08
Simão Quintans <up201504961@fe.up.pt>
2020-12-18 18:15:29 +0000

    Added functions to update new_connection, update green_power, update fast charger availability, update new_measure() function, update start_charging and stop_charging. Added conversion from int to bool to the arguments of the functions.

---
+++ Controlo/BaseDados/teste.py
@@ -0,0 +1,17 @@
+from Controlo.BaseDados.database import database
+import time
+
+if __name__ == '__main__':
+    db = database()
+    # db.new_connection(1,0)
+    # db.update_green_power(1, 1)
+    # db.update_green_power(1,0)
+    # db.start_charging(1, 10, 1, 0, 0, 1)
+    # db.new_measure(1, 1, 20, 5)
+    # db.stop_charging(1, True, 0)
+    # db.start_charging(1, 10, 1, 0, 0, 1)
+    # db.new_measure(1, 1, 20, 5)
+    # time.sleep(3)
+    # db.new_measure(1, 1, 10, 10)
+    # time.sleep(3)
+    # db.stop_charging(1, True, 0)
