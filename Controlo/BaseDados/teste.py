be36eaa41ee1f9df2931f22701c11610f9d1c263
Simão Quintans <up201504961@fe.up.pt>
2020-12-29 12:33:55 +0000

    Added function to check state occupation on database

--- Controlo/BaseDados/teste.py
+++ Controlo/BaseDados/teste.py
@@ -3,10 +3,12 @@ import time
 
 if __name__ == '__main__':
     db = database()
-    db.start_charging(1, 10, 1, 1, 1, 0)
-    db.new_measure(1, 10, 1, 10)
-    time.sleep(36)
+    # av = db.check_availability(202001)
+    # print(av)
+    # db.start_charging(1, 10, 1, 1, 1, 0)
+    # db.new_measure(1, 10, 1, 10)
+    # time.sleep(36)
     # db.new_measure(202007, 10, 1, 5)
     # time.sleep(3)
-    db.stop_charging(1, 1, 0)
+    # db.stop_charging(1, 1, 0)
     # db.reset_chargers()
