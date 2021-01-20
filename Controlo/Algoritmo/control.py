c3973ded3685c174a538b85984856f07cd349519
Francisco de Almeida Pereira <up201605306@fe.up.pt>
2021-01-20 15:13:37 +0000

    Available Power and Green Charge Flag adjustments

--- Controlo/Algoritmo/control.py
+++ Controlo/Algoritmo/control.py
@@ -25,8 +25,8 @@ fastDCPow = chargers_config.fastDCPow
 normalMaxPow = chargers_config.normalPow
 
 # Constants
-INSTALLED_POWER = len(chargers) * normalMaxPow
-
+#INSTALLED_POWER = len(chargers) * normalMaxPow
+INSTALLED_POWER = 100000
 # Flag - Fast Charging Availability
 fastChargAvail = 1 # 1 - Available ; 0 - Not Available
 
@@ -298,6 +298,7 @@ def updateGreenChargAvail():
     else:
         greenChargAvail = 1
 
+    greenChargAvail = 1
 
 def updateFlagsDB(ID):
     # Update Flags
